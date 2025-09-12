2240074dda3a3054038523180d70018d063d8e45
oussamaaouam1 <oussama.aouam@gmail.com>
2025-09-12 12:33:00 +0100

    push chages to production

--- ecom-web-site/app/_utils/axiosClient.js
+++ ecom-web-site/app/_utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl = "http://localhost:1337/api";  
+const apiUrl = process.env.NEXT_PUBLIC_URL;  
 
 if (!apiKey) {
   console.warn("API key is not defined in environment variables");
