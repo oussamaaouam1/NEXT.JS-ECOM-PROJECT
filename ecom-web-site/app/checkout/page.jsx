0974fdd2f3e7e4e6bdf143acf2b15f07679a83ce
oussamaaouam1 <oussama.aouam@gmail.com>
2025-09-12 12:46:37 +0100

    build fo production

--- ecom-web-site/app/checkout/page.jsx
+++ ecom-web-site/app/checkout/page.jsx
@@ -1,27 +1,35 @@
-'use client'
-import React from 'react'
+"use client";
+import React, { Suspense } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from './_components/CheckoutForm';
-import { useSearchParams } from 'next/navigation';
-
+import CheckoutForm from "./_components/CheckoutForm";
+import { useSearchParams } from "next/navigation";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
-console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 
+function CheckoutContent() {
+  const searchParams = useSearchParams();
+  const amount = Number(searchParams.get("amount"));
 
-function Checkout() {
-  const searchParams = useSearchParams()
   const options = {
-    mode:'payment',
-    currency:'usd',
-    amount: Number(searchParams.get('amount'))*100
-  }
+    mode: "payment",
+    currency: "usd",
+    amount: amount * 100,
+  };
+
   return (
     <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm amount={Number(searchParams.get("amount"))} />
+      <CheckoutForm amount={amount} />
     </Elements>
   );
 }
 
-export default Checkout
+function Checkout() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CheckoutContent />
+    </Suspense>
+  );
+}
+
+export default Checkout;
